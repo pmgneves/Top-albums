721dda6504666a7ded4e7008cdfa3826a9da43b6
pmgneves <pedroneves271@gmail.com>
2025-08-27 14:20:01 +0100

    Update spotify-auth.js

--- netlify/functions/spotify-auth.js
+++ netlify/functions/spotify-auth.js
@@ -2,18 +2,21 @@ export async function handler(event, context) {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
   const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
+  const code = event.queryStringParameters.code;
+  const verifier = event.queryStringParameters.verifier;
+
   const body = new URLSearchParams({
     grant_type: "authorization_code",
-    code: event.queryStringParameters.code,
-    redirect_uri: "https://my-best-albums.netlify.app/", // 👈 same as your redirect URI
+    code,
+    redirect_uri: "https://pmgneves.github.io/Top-albums/", // your site URL
+    client_id: clientId,
+    code_verifier: verifier,
   });
 
   const response = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " + Buffer.from(clientId + ":" + clientSecret).toString("base64"),
     },
     body,
   });
